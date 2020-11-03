3a1548b07548e7d743605051a9bf77a6843b7c7e
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-11-03 17:28:04 +0300

    bugs fix

--- script/_getOrderTrack.php
+++ script/_getOrderTrack.php
@@ -7,7 +7,7 @@ require_once("dbconnection.php");
 $id= $_REQUEST['id'];
 $success=0;
 try{
-  $query = "select tracking.*,order_status.status as status,staff.name as staff_name,
+  $query = "select tracking.*,order_status.status as status,if(tracking.staff_id=-1,'???? ??????? ???????', staff.name) as staff_name,
   DATE_FORMAT(tracking.date,'%Y-%m-%d') as date,DATE_FORMAT(tracking.date,'%H:%i') as hour
   from tracking
   left join order_status on tracking.order_status_id = order_status.id
