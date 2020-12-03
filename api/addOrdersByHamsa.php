e638c924a6e3c5b55e9fce9603b489db554fe96a
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-12-03 19:08:11 +0300

    U

--- api/addOrdersByHamsa.php
+++ api/addOrdersByHamsa.php
@@ -154,7 +154,7 @@ if($v->passes()) {
                           $client,$store,$val['customer_name'],
                           $val['customer_phone'],$val['city_id'],$val['town_id'],$to_branch,$with_dev,$val['note'],$new_price,$val['address'],$company,$confirm,$val['id']]);
            if($result > 1){
-             $data[] = ['barcode'=>$result,'id'=>0,'order_no'=>$val['order_no'],'driver_phone'=>$driver_phone];
+             $data[] = ['barcode'=>$result,'id'=>$val['id'],'order_no'=>$val['order_no'],'driver_phone'=>$driver_phone];
              $success = 1;
            }
             $add++;
