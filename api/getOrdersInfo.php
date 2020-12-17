bcfbcc5e77c014bc4e13f6fd28fa6e70942e4ddf
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-12-17 17:09:15 +0300

    New api

---
+++ api/getOrdersInfo.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+header('Content-Type: application/json');
+error_reporting(0);
+require_once("_apiAccess.php");
+access();
+$data=["No Data"];
+$success="0";
+$token = $_REQUEST['token'];
+$orders = $_REQUEST['bar_codes'];
+  if(count($orders) > 0){
+    if(count($orders) > 0){
+        $a = 0;
+        foreach($ids as $id){
+          if($a==0){
+            if(is_int($id)){
+              $f = " orders.id =".$id;
+            }
+          }else{
+            if(is_int($id)){
+              $f .= " or orders.id =".$id;
+            }
+          }
+          $a++;
+       }
+       $f = " and ( ".$f." )";
+    }
+  require_once("../script/dbconnection.php");
+  try{
+    $query = "select confirm, order_status_id as status from orders where client_id='".$clinetdata['id']."'  ".$f;
+    $data = getData($con,$query);
+    $success="1";
+  } catch(PDOException $ex) {
+     $data=["error"=>$ex];
+     $success="0";
+  }
+}
+echo json_encode(["success"=>$success,"data"=>$data,'messgae'=>""]);
+?>
