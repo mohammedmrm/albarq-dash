d025ee1e16fe81c9fe81c002681ac5d64232728e
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-12-27 20:41:29 +0300

    bugs

--- api/addOrdersByHamsa.php
+++ api/addOrdersByHamsa.php
@@ -2,9 +2,10 @@
 ob_start();
 session_start();
 error_reporting(0);
-header("Access-Control-Allow-Headers");
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+
 require_once("_apiAccess.php");
 access();
 require_once("../script/dbconnection.php");
--- api/getOrdersInfo.php
+++ api/getOrdersInfo.php
@@ -1,9 +1,9 @@
 <?php
 ob_start();
 session_start();
-header("Access-Control-Allow-Headers");
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 error_reporting(0);
 require_once("_apiAccess.php");
 access();
