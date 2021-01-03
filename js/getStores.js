6009b1c9c377ac6c2634a48b7d198fedc0067e01
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2021-01-03 13:42:20 +0300

    update

--- js/getStores.js
+++ js/getStores.js
@@ -1,22 +1,28 @@
-﻿function getStores(elem,client){
-   $.ajax({
-     url:"script/_getStores.php",
-     type:"POST",
-     data:{client: client},
-     success:function(res){
-       elem.html("");
-       elem.append(
-           '<option value="">... اختر ...</option>'
-       );
-       $.each(res.data,function(){
-         elem.append("<option value='"+this.id+"'><b>"+this.name+"</b>-"+this.client_name+"</option>");
-       });
-       console.log(res);
-       elem.selectpicker('refresh');
-     },
-     error:function(e){
-        elem.append("<option value='' class='bg-danger'>Error</option>");
-        console.log(e);
-     }
-   });
+﻿function getStores(elem, client) {
+  $.ajax({
+    url: "script/_getStores.php",
+    type: "POST",
+    data: { client: client },
+    success: function (res) {
+      elem.html("");
+      elem.append('<option value="">... اختر ...</option>');
+      $.each(res.data, function () {
+        elem.append(
+          "<option value='" +
+            this.id +
+            "'><b>" +
+            this.name +
+            "</b>-" +
+            this.client_name +
+            "</option>"
+        );
+      });
+      console.log(res);
+      elem.selectpicker("refresh");
+    },
+    error: function (e) {
+      elem.append("<option value='' class='bg-danger'>Error</option>");
+      console.log(e);
+    },
+  });
 }
--- pages/clientsDetails.php
+++ pages/clientsDetails.php
@@ -716,8 +716,36 @@ function unpayInvoice(id){
       });
   }
 }
+function getStoresOrdered(elem, client) {
+  $.ajax({
+    url: "script/_getStoresOrdered.php",
+    type: "POST",
+    data: { client: client },
+    success: function (res) {
+      elem.html("");
+      elem.append('<option value="">... اختر ...</option>');
+      $.each(res.data, function () {
+        elem.append(
+          "<option value='" +
+            this.id +
+            "'><b>" +
+            this.name +
+            "</b>-" +
+            this.client_name +
+            "</option>"
+        );
+      });
+      console.log(res);
+      elem.selectpicker("refresh");
+    },
+    error: function (e) {
+      elem.append("<option value='' class='bg-danger'>Error</option>");
+      console.log(e);
+    },
+  });
+}
 $( document ).ready(function(){
-getStores($("#store"));
+getStoresOrdered($("#store"));
 $("#allselector2").change(function() {
     var ischecked= $(this).is(':checked');
     if(!ischecked){
