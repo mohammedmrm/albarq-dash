b972f80451b7a35b62cb0531c5028115ffcff11f
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2021-04-29 13:10:03 +0200

    update

--- charts/_getOrdersCount.php
+++ charts/_getOrdersCount.php
@@ -78,7 +78,7 @@ foreach($res as $val){
               UPDATE
               orders SET order_status_id = 4
               WHERE (order_status_id = 3 or order_status_id = 1) and driver_id > 0 and invoice_id = 0 and driver_invoice_id = 0 and confirm=1 and to_city = '".$val['city_id']."' and
-              DATE(date) < DATE_SUB(CURDATE(), INTERVAL ".$val['days']." DAY) AND ( SELECT @uids := CONCAT_WS(',', id, @uids)) limit 500;
+              DATE(date) < DATE_SUB(CURDATE(), INTERVAL ".$val['days']." DAY) AND ( SELECT @uids := CONCAT_WS(',', id, @uids)) limit 200;
               SELECT @uids as ids;";
    $ids = getAllUpdatedIds($mysqlicon,$auto);
    $idss = explode (",", $ids[0][0]);
@@ -113,7 +113,7 @@ foreach($res as $val){
               UPDATE
               orders SET order_status_id = 4
               WHERE (order_status_id = 7) and driver_id > 0 and invoice_id = 0 and driver_invoice_id = 0 and confirm=1 and to_city = '".$val['city_id']."' and
-              DATE(date) < DATE_SUB(CURDATE(), INTERVAL ".$val['p_days']." DAY) AND ( SELECT @uids := CONCAT_WS(',', id, @uids)) limit 500;
+              DATE(date) < DATE_SUB(CURDATE(), INTERVAL ".$val['p_days']." DAY) AND ( SELECT @uids := CONCAT_WS(',', id, @uids)) limit 100;
               SELECT @uids as ids;";
    $idsp = getAllUpdatedIds($mysqlicon,$auto);
    $idss = explode (",", $idsp[0][0]);
@@ -147,7 +147,7 @@ foreach($res as $val){
               UPDATE
               orders SET order_status_id = 4
               WHERE (order_status_id = 9) and driver_id > 0 and invoice_id = 0 and driver_invoice_id = 0 and confirm=1 and to_city = '".$val['city_id']."' and
-              DATE(date) < DATE_SUB(CURDATE(), INTERVAL ".$val['r_days']." DAY) AND ( SELECT @uids := CONCAT_WS(',', id, @uids)) limit 500;
+              DATE(date) < DATE_SUB(CURDATE(), INTERVAL ".$val['r_days']." DAY) AND ( SELECT @uids := CONCAT_WS(',', id, @uids)) limit 100;
               SELECT @uids as ids;";
    $ids = getAllUpdatedIds($mysqlicon,$auto);
    $idss = explode (",", $ids[0][0]);
